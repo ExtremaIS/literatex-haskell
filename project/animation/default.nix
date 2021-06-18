874ecbc2b4ef63000c344f6d36f48221fc91fee7
Divam <dfordivam@gmail.com>
2021-06-18 16:08:56 +0900

    Simplify literate-animation nix, and add proper shell.nix

--- project/animation/default.nix
+++ project/animation/default.nix
@@ -1,35 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
 let
-
-  nixpkgsRev = "2c162d49cd5b979eb66ff1653aecaeaa01690fcc";
-  compilerVersion = "ghc884";
-
-  githubTarball = owner: repo: rev:
-    builtins.fetchTarball { url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; };
-
   gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
-
-  config = {
-    packageOverrides = super: let self = super.pkgs; in rec {
-      haskell = super.haskell // {
-        packageOverrides = self: super: {
-          haskell-nix = super.callCabal2nix "haskell-nix" (gitIgnore [./.gitignore] ./.) {};
-        };
-      };
-    };
-  };
-
-  pkgs = import (githubTarball "NixOS" "nixpkgs" nixpkgsRev) { inherit config; };
-  compilerSet = pkgs.haskell.packages."${compilerVersion}";
-
-in {
-
-  inherit pkgs;
-
-  shell = compilerSet.shellFor {
-    packages = p: [p.haskell-nix];
-    buildInputs = with pkgs; [
-      compilerSet.cabal-install
-    ];
-  };
-
-}
+in
+  pkgs.haskellPackages.developPackage {
+    root = gitIgnore [./.gitignore] ./.;
+    name = "literatex-animation";
+  }
