5cc3479cf093e29f39d0d619db479fe8c9326557
Travis Cardwell <travis.cardwell@extrema.is>
2021-06-23 08:35:15 +0900

    Refactor Nix configuration

--- project/animation/default.nix
+++ project/animation/default.nix
@@ -7,8 +7,8 @@
 #     $ nix-build
 
 { # This string argument specifies the compiler (example: "ghc8104").  When
-  # not specified, `ghc8104` is used.
-  compiler ? "ghc8104"
+  # not specified, the default compiler (configured below) is used.
+  compiler ? null
   # This path argument specifies the packages to use.  When not specified and
   # the default compiler is selected, a known good revision is used.
   # Otherwise, the packages configured on the filesystem are used.
@@ -20,28 +20,37 @@
 
 let
 
+  # This string defines the default compiler version.
+  defaultCompiler = "ghc8104";
+
+  # This string defines a known good revision for the default compiler.
+  defaultRevision = "4d4fdc329285e0d0c1c1a2b65947d651b8ba6b29";
+
   # This function fetches the specified nixpkgs revision.
   nixpkgsTarball = rev:
     builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     };
 
+  # The compiler is explicitly specified or the default.
+  compiler' = if isNull compiler then defaultCompiler else compiler;
+
   # Packages are explicitly specified, those for a known good revision for the
   # default compiler, or those configured on the filesystem.
   pkgs = if isNull nixpkgs
-    then if compiler == "ghc8104"
-      then import (nixpkgsTarball "4d4fdc329285e0d0c1c1a2b65947d651b8ba6b29") {}
+    then if isNull compiler
+      then import (nixpkgsTarball defaultRevision) {}
       else import <nixpkgs> {}
     else nixpkgs;
 
-  # Git ignore functionality must use the most recent revision.
+  # This does not work with old revisions.
   gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
 
 in
 
   # Configure the development environment for the package using the selected
   # packages and compiler.
-  pkgs.haskellPackages.developPackage {
+  pkgs.haskell.packages.${compiler'}.developPackage {
     root = gitIgnore [./.gitignore] ./.;
     name = "literatex-animation";
     modifier = drv:
--- project/animation/shell.nix
+++ project/animation/shell.nix
@@ -7,8 +7,8 @@
 #     $ nix-shell
 
 { # This string argument specifies the compiler (example: "ghc8104").  When
-  # not specified, `ghc8104` is used.
-  compiler ? "ghc8104"
+  # not specified, the default compiler is used.
+  compiler ? null
   # This path argument specifies the packages to use.  When not specified and
   # the default compiler is selected, a known good revision is used.
   # Otherwise, the packages configured on the filesystem are used.
