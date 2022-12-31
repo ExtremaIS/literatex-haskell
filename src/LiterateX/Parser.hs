9fc236b152cf3a0b1c9133a21665841d5ac6dc5f
Travis Cardwell <travis.cardwell@extrema.is>
2023-01-01 07:53:25 +0900

    Update copyright date

--- src/LiterateX/Parser.hs
+++ src/LiterateX/Parser.hs
@@ -2,7 +2,7 @@
 -- |
 -- Module      : LiterateX.Parser
 -- Description : source parser
--- Copyright   : Copyright (c) 2021-2022 Travis Cardwell
+-- Copyright   : Copyright (c) 2021-2023 Travis Cardwell
 -- License     : MIT
 --
 -- This module implements the source parser.
